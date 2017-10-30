6deec74ec08087f686cef7414c01f0313d0d4225
Jstuff36 <mizzou_fan10@hotmail.com>
2017-10-29 20:46:09 -0400

    Finish react typescript app

---
+++ src/actions/index.tsx
@@ -0,0 +1,30 @@
+import * as constants from '../constants'
+
+export interface IncrementEnthusiasm {
+    type: constants.INCREMENT_ENTHUSIASM;
+}
+
+export interface DecrementEnthusiasm {
+    type: constants.DECREMENT_ENTHUSIASM;
+}
+
+// The pipe | says the variable can be either type
+export type EnthusiasmAction = IncrementEnthusiasm | DecrementEnthusiasm;
+
+export function incrementEnthusiasm(): IncrementEnthusiasm {
+    return {
+        type: constants.INCREMENT_ENTHUSIASM
+    }
+}
+
+export function decrementEnthusiasm(): DecrementEnthusiasm {
+    return {
+        type: constants.DECREMENT_ENTHUSIASM
+    }
+}
+
+// export const decrementEnthusiasm = (): DecrementEnthusiasm => {
+//     return {
+//         type: constants.DECREMENT_ENTHUSIASM
+//     }
+// }
